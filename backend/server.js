44b1605046091ceec85f0b48ac119447e93fa3ab
erickcruz1031@gmail.com <erickcruz1031@gmail.com>
2021-04-19 18:03:48 -0700

    Set up the routes and tested requests

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,10 @@ connection.once('open', () =>{
 })
 
 
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
+
 
 app.listen(port, () =>{
     console.log('Server is running on port:', port);
