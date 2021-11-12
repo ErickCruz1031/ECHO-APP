839065c8ac878bddeaad7094e263a631a3823059
Erick  Jaimez <erickcruz1031@gmail.com>
2021-11-11 17:17:12 -0800

    Adding configurations to use Auth0

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const { auth } = require('express-openid-connect');
 require('dotenv').config();
 
 const app= express();
 const port = process.env.PORT || 5000;
+
+app.use(
+  auth({
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+    idpLogout: true,
+  })
+);
+
+
 
 app.use(cors());
 app.use(express.json());
