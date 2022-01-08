e746f8b2326656c5242e0ca14778ddde9cd5c15e
Erick  Jaimez <erickcruz1031@gmail.com>
2022-01-08 14:54:29 -0800

    Fixed the issue with the clickaway listener on the search box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/NavBar';
+// routes
+import Router from './routes';
+// theme
+import ThemeConfig from './theme';
+import GlobalStyles from './theme/globalStyles';
+// components
+import ScrollToTop from './components/ScrollToTop';
+import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
+import LoginButton from'./components/LoginButton';
+import Profile from './components/Profile'
+import DashboardApp from './pages/DashboardApp';
+import DashboardLayout from './layouts/dashboard';
+// ----------------------------------------------------------------------
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <ThemeConfig>
+    <ScrollToTop />
+    <GlobalStyles />
+    <BaseOptionChartStyle />
+    <Router />
+  </ThemeConfig>
+
   );
 }
 
-export default App;
+/*
+
+
+export default function App() {
+  return (
+    <ThemeConfig>
+      <ScrollToTop />
+      <GlobalStyles />
+      <LoginButton />
+      <Profile />
+      <BaseOptionChartStyle />
+      <Router />
+    </ThemeConfig>
+  );
+}
+
+
+
+    <ThemeConfig>
+      <ScrollToTop />
+      <GlobalStyles />
+      <BaseOptionChartStyle />
+      <DashboardLayout/>
+    </ThemeConfig>
+*/
