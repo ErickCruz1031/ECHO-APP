e3608d15489bc9d10b37c0a4ec765161a5e4276b
Erick  Jaimez <erickcruz1031@gmail.com>
2022-01-08 15:09:46 -0800

    Added back the clickAway listener

--- frontend/src/pages/DashboardApp.js
+++ frontend/src/pages/DashboardApp.js
@@ -58,9 +58,6 @@ export default function DashboardApp() {
           <Grid item xs={12} md={6} lg={8}>
             <Profile />
           </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentSubject />
-          </Grid>
 
           <Grid item xs={12} md={6} lg={8}>
             <AppNewsUpdate />
@@ -70,29 +67,8 @@ export default function DashboardApp() {
             <AppOrderTimeline />
           </Grid>
 
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTrafficBySite />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWeeklySales />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <AppNewUsers />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <AppItemOrders />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <AppBugReports />
-          </Grid>
-
           <Grid item xs={12} md={6} lg={8}>
             <AppWebsiteVisits />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentVisits />
           </Grid>
 
           <Grid item xs={12} md={6} lg={8}>
